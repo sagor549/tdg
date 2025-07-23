4d10514f3131d017aab5f0b364b0fef9f5919ebf
sagor549 <sagorghosh9899@gmail.com>
2025-07-23 21:56:17 +0530

    fgnrnf

--- src/components/BrandCreationPreview.jsx
+++ src/components/BrandCreationPreview.jsx
@@ -60,7 +60,7 @@ const BrandCreationPreview = () => {
         playsInline
         className="absolute top-0 left-0 w-full h-full object-cover z-0"
       >
-        <source src="/assets/bgvid.webm" type="video/mp4" />
+        <source src="/assets/bgvid.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
